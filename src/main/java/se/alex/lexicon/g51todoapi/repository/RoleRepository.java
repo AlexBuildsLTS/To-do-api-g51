24cf52b36f17fe3b9f19aa7c5d06872533cdd07c
ALex Youssef <ALex.youssef@live.com>
2024-10-03 22:37:06 +0200

    Added repository interfaces for entities: Person, Task, and User

---
+++ src/main/java/se/alex/lexicon/g51todoapi/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package se.alex.lexicon.g51todoapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository<role> extends JpaRepository<role, Long> {
+
+}
